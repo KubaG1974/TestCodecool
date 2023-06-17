b815df506851862d819fb6694684025d68db3f99
KubaG1974 <kuba.gorzynski@gmail.com>
2023-06-17 13:02:37 +0200

    Updated Hanger.cs

--- TestCodecool/TestCodecool/Hanger.cs
+++ TestCodecool/TestCodecool/Hanger.cs
@@ -1,6 +1,9 @@
+using System.Collections.Generic;
+
 namespace TestCodecool;
 
 public class Hanger
 {
-    
+    public bool isEmpty { get; set; }
+    public List<Clothing> Clothes { get; set; }
 }
