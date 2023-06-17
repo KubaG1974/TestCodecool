481f50cb9c7b1d60439f315997d725bc36537715
KubaG1974 <kuba.gorzynski@gmail.com>
2023-06-17 13:31:39 +0200

    Added HangClothing and CanHangOnSameHanger to Wardrobe class - sorry for stupid names

--- TestCodecool/TestCodecool/Wardrobe.cs
+++ TestCodecool/TestCodecool/Wardrobe.cs
@@ -11,5 +11,44 @@ public class Wardrobe
         {
             hangers.Add(new Hanger());
         }
-    }    
+    }
+
+    public bool HangClothing(Clothing clothing)
+    {
+        foreach (Hanger hanger in hangers)
+        {
+            if (hanger.IsEmpty)
+            {
+                if (hanger.Clothes == null)
+                {
+                    hanger.Clothes = new List<Clothing>();
+                }
+
+                hanger.Clothes.Add(clothing);
+                hanger.IsEmpty = false;
+                return true;
+            }
+            else if (CanHangOnSameHanger(hanger, clothing))
+            {
+                hanger.Clothes.Add(clothing);
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    private bool CanHangOnSameHanger(Hanger hanger, Clothing clothing)
+    {
+        if (clothing.Type == "shirt" || clothing.Type == "blouse")
+        {
+            return hanger.Clothes != null && hanger.Clothes.Exists(c => c.Type == "shirt" || c.Type == "blouse");
+        }
+        else if (clothing.Type == "trousers" || clothing.Type == "skirt")
+        {
+            return hanger.Clothes != null && hanger.Clothes.Exists(c => c.Type == "trousers" || c.Type == "skirt");
+        }
+
+        return false;
+    }
 }
