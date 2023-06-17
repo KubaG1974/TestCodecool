2abfb9cd3a42e47c540ce0a9da63e13a4d535f40
KubaG1974 <kuba.gorzynski@gmail.com>
2023-06-17 13:22:06 +0200

    Added capacity of wardrobe

--- TestCodecool/TestCodecool/Wardrobe.cs
+++ TestCodecool/TestCodecool/Wardrobe.cs
@@ -2,5 +2,14 @@ namespace TestCodecool;
 
 public class Wardrobe
 {
-    
+    private List<Hanger> hangers;
+
+    public Wardrobe(int capacity)
+    {
+        hangers = new List<Hanger>(capacity);
+        for (int i = 0; i < capacity; i++)
+        {
+            hangers.Add(new Hanger());
+        }
+    }    
 }
