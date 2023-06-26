a9b22b78c7938e0b5830144a1804ca3327757df0
KubaG1974 <kuba.gorzynski@gmail.com>
2023-06-26 08:19:22 +0200

    Updated all files with OOP principles

--- TestCodecool/TestCodecool/Hanger.cs
+++ TestCodecool/TestCodecool/Hanger.cs
@@ -5,7 +5,7 @@ namespace TestCodecool;
 public abstract class Hanger
 {
     public bool IsEmpty { get; protected set; }
-    public List<Clothing> Clothes { get; protected set; }
+    public List<Clothing>? Clothes { get; protected set; }
 
     public abstract bool CanHangClothing(Clothing clothing);
     public abstract void HangClothing(Clothing clothing);
